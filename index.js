9cba828e3cc748b958ff6d009ba027f69d2e5924
Simon9116 <simoneleveghi8@protonmail.com>
2024-11-22 20:44:32 +0100

    Add env variables to switch between dev and prod mode

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const express = require('express');
+const https = require('https');
 const { join } = require('path');
+const { readFileSync } = require("fs");
+require('dotenv').config();
 
 const app = express();
 
-const port = 3000;
-const hostname = 'localhost';
+const hostname = (process.env.STATUS === "production")? process.env.PROD_HOST : process.env.DEV_HOST;
+const port = (process.env.STATUS === "production")? process.env.PROD_PORT : process.env.DEV_PORT;
 
 app.get('/', (req, res) => {
     res.sendFile(join(__dirname, 'index.html'));
 });
 
-app.listen(port, hostname, () => {
-    console.log(`Listening on http://${hostname}:${port}`);
-})
-
+if(process.env.STATUS === "production") {
+    https.createServer({
+        key: readFileSync(join(__dirname, 'cert/server.key')),
+        cert: readFileSync(join(__dirname, 'cert/server.cert'))
+    }, app).listen(443, () => {
+        console.log(`Listening at: https://${hostname}:${port}`);
+    });
+}
+else {
+    app.listen(port, hostname, () => {
+        console.log(`Listening at http://${hostname}:${port}`);
+    })
+}
