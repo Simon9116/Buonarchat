6e3836260ae33e8ed1c918dcdb192c428ee789e0
Simon9116 <115188462+Simon9116@users.noreply.github.com>
2025-05-27 11:58:51 +0200

    Add basic chats list

--- index.js
+++ index.js
@@ -79,11 +79,20 @@ app.post('/signup', (req, res) => {
 })
 
 app.get('/chat/:chatId', (req, res) => {
+    let chats;
+    con.prepare("SELECT c.id, c.name FROM UserChat uc JOIN Chat c ON uc.chat = c.id WHERE userAccount=?", (err, stmt) => {
+        if (err) throw err;
+        stmt.execute([req.session.user.id], (err, result) => {
+            if (err) throw err;
+            chats = result;
+        })
+    })
+
     con.prepare("SELECT * FROM Message WHERE chat=?", (err, stmt) => {
         if (err) throw err;
         stmt.execute([req.params.chatId], (err, result) => {
             if (err) throw err;
-            res.render('chat', {messages: result, chatId: req.params.chatId, userId: req.session.user.id});
+            res.render('chat', {chats: chats, messages: result, chatId: req.params.chatId, userId: req.session.user.id});
         })
     })
 })
