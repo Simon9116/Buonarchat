f0863af21cfb716ef922a5816634252254ff7b1e
Simon9116 <simoneleveghi8@protonmail.com>
2025-05-29 22:33:55 +0200

    Handle GET requests to non-joined chats

--- routes/chat.js
+++ routes/chat.js
@@ -3,11 +3,25 @@ const conPromise = require("../connection");
 
 const router = express.Router();
 
+async function fetchChats(userId) {
+    const con = await conPromise;
+    const [chats, fields] = await con.execute("SELECT c.id, c.name FROM UserChat uc JOIN Chat c ON uc.chat = c.id WHERE userAccount=?", [userId]);
+
+    return chats;
+}
+
+router.get("/", async (req, res) => {
+    const chats = await fetchChats(req.session.user.id);
+    res.render('chat-index', {chats: chats});
+});
+
 router.get('/:chatId', async (req, res) => {
     const con = await conPromise;
 
-    const [chats, fields1] = await con.execute("SELECT c.id, c.name FROM UserChat uc JOIN Chat c ON uc.chat = c.id WHERE userAccount=?", [req.session.user.id]);
+    const [userChats, fields] = await con.execute("SELECT 1 FROM UserChat WHERE userAccount=? AND chat=?", [req.session.user.id, req.params.chatId]);
+    if(userChats.length === 0) return res.redirect("/chat");
 
+    const chats = await fetchChats(req.session.user.id);
     const [messages, fields2] = await con.execute("SELECT * FROM Message WHERE chat=?", [req.params.chatId]);
     res.render('chat', {chats: chats, messages: messages, chatId: req.params.chatId, userId: req.session.user.id});
 });
